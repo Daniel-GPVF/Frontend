d819cb58bcd62316156501aa6f141211aa41c3e6
Daniel-GPVF <daniel.gilpelle@vodafone.com>
2025-10-14 10:28:45 +0200

    14.10.25

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+function klickreaction (parameter) {
+    theDiv.style.color = "red";
+    theDiv.style.backgroundColor = "blue"
+    theDiv2.style.color = "white"
+}
+
+let theLink = document.getElementById("link1");
+document.getElementById("link1").innerHTML = "neuer Text";
+theLink.innerHTML = "neuster Text";
+theLink.style.color = "red";
+theLink.style.backgroundColor = "white"
+theLink.style.fontSize = "30pt"
+
+let theDiv = document.getElementById("div1")
+document.getElementById("link1").addEventListener("click", klickreaction);
+
+let theDiv2 = document.getElementById("div2")
+document.getElementById("link1").addEventListener("click", klickreaction);
