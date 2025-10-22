967e5bb25ad2b00e72ab61f38fc931d5a67eacef
Daniel-GPVF <daniel.gilpelle@vodafone.com>
2025-10-22 14:06:31 +0200

    Array & for-loop Übung

--- js/script.js
+++ js/script.js
@@ -20,6 +20,7 @@ let theDiv2 = document.getElementById("div2")
 document.getElementById("link1").addEventListener("click", klickreaction);
 */
 
+/*
 // --- dark mode toggle ---
 
 const checkbox = document.getElementById("checkbox");
@@ -28,3 +29,28 @@ checkbox.addEventListener("change", () => {
     // change the theme of the website
     document.body.classList.toggle("dark");
 });
+
+let 
+*/
+
+// --- array & for loop --- 
+
+const charactername = ["akuma", "ryu", "chun-li", "ken", "m.bison", "cammy", "vega", "sagat", "balrog", "dhalsim", "blanka", "zangief", "e.honda", "guile", "thawk", "feilong", "deejay", "t.hawk", "sakura", "oni", "evil ryu", "juri", "cody", "gouken", "gill"];
+document.getElementById("demo-liste").innerHTML = charactername;
+
+const images = [Image1 = "../img/akuma.png", Image2 = "../img/ryu.png", Image3 = "../img/chun-li.png"];
+
+// was letzt endlich ausgespielt wird
+let character = "";
+for (let i = 0; i < charactername.length; i++) {
+    character += "<span class='sf6'>" + (i+1) + ".) " + charactername[i] + "</span>" + "<br>";
+}
+
+document.getElementById("demo-liste").innerHTML = character;
+
+let imageList = "";
+for (let i = 0; i < images.length; i++) {
+    imageList += "<img src='" + images[i] + "' alt='character image' width='100px' height='100px'>" + " ";
+}
+
+document.getElementById("demo-images").innerHTML = imageList;
